9c70277f3858ce2b233de464843422a1a5673791
Grant Jenks <grant.jenks@gmail.com>
2022-12-31 17:46:54 -0800

    Copy demo code from chat gpt
    Initial vignere cipher

--- src/knightswhosayni/crypto.py
+++ src/knightswhosayni/crypto.py
@@ -46,3 +46,71 @@ def ninini_search(name):
 
 
 codecs.register(ninini_search)
+
+
+def vignere_cipher_encode(message, key):
+    # Convert the message and key to a list of characters
+    message = list(message)
+    key = list(key)
+
+    # Make the key the same length as the message
+    key = key * (len(message) // len(key)) + key[:len(message) % len(key)]
+
+    # Zip the message and key together and apply the cipher
+    result = []
+    for (c, k) in zip(message, key):
+        if chr(36) <= c <= chr(126):
+            # Get the character's ASCII code
+            c_code = ord(c)
+            k_code = ord(k)
+
+            # Encode the character
+            c_code = (c_code + k_code - 36) % 91 + 36
+
+            # Convert the character back to a letter
+            c = chr(c_code)
+
+        # Append the character to the result
+        result.append(c)
+
+    # Convert the result back to a string and return it
+    return ''.join(result)
+
+def vignere_cipher_decode(message, key):
+    # Convert the message and key to a list of characters
+    message = list(message)
+    key = list(key)
+
+    # Make the key the same length as the message
+    key = key * (len(message) // len(key)) + key[:len(message) % len(key)]
+
+    # Zip the message and key together and apply the cipher
+    result = []
+    for (c, k) in zip(message, key):
+        if chr(36) <= c <= chr(126):
+            # Get the character's ASCII code
+            c_code = ord(c)
+            k_code = ord(k)
+
+            # Decode the character
+            c_code = (c_code - k_code + 36) % 91 + 36
+
+            # Convert the character back to a letter
+            c = chr(c_code)
+
+        # Append the character to the result
+        result.append(c)
+
+    # Convert the result back to a string and return it
+    return ''.join(result)
+
+# Example usage:
+
+message = "HELLO"
+key = "WORLD"
+
+encoded_message = vignere_cipher_encode(message, key)
+print(encoded_message) # Output: QGNNQ
+
+decoded_message = vignere_cipher_decode(encoded_message, key)
+print(decoded_message) # Output: HELLO
