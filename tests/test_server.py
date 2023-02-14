f84b6debbd842951b8e917940bd4496ff1dcec6f
Grant Jenks <grant.jenks@gmail.com>
2023-02-13 23:23:31 -0800

    Scratch code for manual testing

---
+++ tests/test_server.py
@@ -0,0 +1,12 @@
+from knightswhosayni.models import Project, Key, License
+
+
+project = Project(name='Django Rrweb', slug='django-rrweb')
+project.save()
+
+key = Key(
+    project=project,
+    value='b5f570e0-6585-402c-b344-3d0521dc8740',
+    prefix='DJANGO_RRWEB_',
+)
+key.save()
